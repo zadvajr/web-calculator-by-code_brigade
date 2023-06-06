c0acc9b1bc391e28af33a0c9a6b703bfcc27ec59
zadvajr <danielzadva@gmail.com>
2023-06-06 20:36:54 +0100

    added display() to set buttons value to screen

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,10 @@
 /*This is the code logic, computational and interactivity  for the web-calculator. 
 @zadvajr and @Dannyross1 are responsible for this part*/
 
+// display() sets buttons value to display
+
+
+function display(value) {
+    document.getElementById("screen").value += value;;
+    console.log(buttonValues.value);
+}
